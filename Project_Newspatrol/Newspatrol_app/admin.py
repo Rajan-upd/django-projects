42713621118bc32e0b343177e24704a87e35d6a6
Rajan Upadhyaya <93195376+Rajan-upd@users.noreply.github.com>
2022-05-06 06:29:38 +0545

    This is my first project.

---
+++ Project_Newspatrol/Newspatrol_app/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from Newspatrol_app.models import News
+
+# Register your models here.
+admin.site.register(News)
---
+++ Project_Newspatrol/Newspatrol_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NewspatrolAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Newspatrol_app'
